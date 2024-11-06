94293c597909df6b8715f752b83e9a80fbbf5a5f
izi Achat <iziachat@gmail.com>
2024-11-06 07:46:39 -0500

    add proper chasier name

--- app/(store)/pos/page.tsx
+++ app/(store)/pos/page.tsx
@@ -3,6 +3,7 @@ import { Suspense } from 'react';
 import Cart from './components/cart';
 import ProductGrid from './components/product-grid';
 import getItems from './get-items';
+import { currentUser } from '@clerk/nextjs/server';
 
 interface Props {
   searchParams: { searchQuery: string };
@@ -11,8 +12,8 @@ interface Props {
 const PosPage = async ({ searchParams: { searchQuery } }: Props) => {
   const items = await getItems({ search: searchQuery });
 
-  // const user = await currentUser();
-  // const userName = user?.fullName || user?.username;
+  const user = await currentUser();
+  const userName = user?.fullName || user?.username;
 
   return (
     <div>
@@ -24,7 +25,7 @@ const PosPage = async ({ searchParams: { searchQuery } }: Props) => {
           <ProductGrid products={items} />
         </div>
         <div className='space-y-6 w-[20rem] h-[87vh]  rounded-lg  p-4 border-[0.1px] shadow-sm'>
-          <Cart cashier={'Hello'} />
+          <Cart cashier={userName!} />
         </div>
       </div>
     </div>

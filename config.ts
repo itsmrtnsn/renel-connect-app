e2120fd364df94d54bf450b5a4df9d7e4055bc99
izi Achat <iziachat@gmail.com>
2024-12-09 12:25:55 -0500

    add

--- config.ts
+++ config.ts
@@ -4,3 +4,7 @@
 // DATABASE_URL_UNPOOLED="postgresql://renelconnectdb_owner:OCBaYU9NX8rR@ep-winter-truth-a62audd2.us-west-2.aws.neon.tech/renelconnectdb?sslmode=require"
 // NEXT_PUBLIC_CLERK_SIGN_IN_URL=/sign-in
 // NEXT_PUBLIC_CLERK_SIGN_UP_URL=/sign-up
+
+// DATABASE_URL="postgresql://neondb_owner:bUhgi1IHR2Be@ep-damp-dawn-a59ahw9j-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require"
+// # uncomment next line if you use Prisma <5.10
+// # DATABASE_URL_UNPOOLED="postgresql://neondb_owner:bUhgi1IHR2Be@ep-damp-dawn-a59ahw9j.us-east-2.aws.neon.tech/neondb?sslmode=require"
